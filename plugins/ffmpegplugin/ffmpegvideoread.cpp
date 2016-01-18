70532748bab98dfc2dad3eb5fec6fdb39ef6f180
Alex <devkral@web.de>
2016-01-18 17:21:39 +0100

    fix build

--- plugins/ffmpegplugin/ffmpegvideoread.cpp
+++ plugins/ffmpegplugin/ffmpegvideoread.cpp
@@ -80,7 +80,8 @@ void ffmpegvideoread::enter(const std::vector<sgstreamspec*> &in,const std::vect
 	this->getManager()->updateStreamspec(out[0], new spec_ffmpegi(this->cod_context, 4));
 
 	ffmpeg::av_init_packet(&this->packet);
-	this->frame = ffmpeg::av_frame_alloc();
+	this->origframe = ffmpeg::av_frame_alloc();
+	this->convertframe = ffmpeg::av_frame_alloc();
 	this->intern_thread=new std::thread(sgactor::thread_wrapper, this);
 }
 void ffmpegvideoread::run(const std::vector<std::shared_ptr<sgstream>> )
@@ -90,13 +91,13 @@ void ffmpegvideoread::run(const std::vector<std::shared_ptr<sgstream>> )
 	if (ffmpeg::av_read_frame(this->form_context, &this->packet)<0)
 		return;
 	// dismiss wrong packets
-	std::cout << "read1\n";
+	//std::cout << "read1\n";
 	while(packet.stream_index!=this->video_stream_index)
 	{
 		if (ffmpeg::av_read_frame(this->form_context, &this->packet)<0)
 			return;
 	}
-	std::cout << "read2\n";
+	//std::cout << "read2\n";
 
 
 	//this->input_device_format->read_packet(this->form_context, this->packet);
@@ -105,13 +106,13 @@ void ffmpegvideoread::run(const std::vector<std::shared_ptr<sgstream>> )
 	if (this->got_frame==0)
 		return;
 	int size = ffmpeg::av_image_get_buffer_size(ffmpeg::AV_PIX_FMT_RGBA64, this->origframe->width, this->origframe->height, 0);
-	std::cout << size << std::endl;
-	uint8_t *buffer=(uint8_t*)calloc(sizeof(uint8_t),size);
-	ffmpeg::sws_scale(sws_ctx, (uint8_t const * const *)this->frame->data,
-			 this->frame->linesize, 0, this->cod_context->height,
-			 pFrameRGB->data, pFrameRGB->linesize)
-	ffmpeg::av_image_copy_to_buffer(buffer,size, this->frame->data, this->frame->linesize, ffmpeg::AV_PIX_FMT_RGBA64, this->frame->width, this->frame->height, 0);
-	this->streamsout[0]->updateStream(new stream_data(buffer, size));
+	//std::cout << size << std::endl;
+	//uint8_t *buffer=(uint8_t*)calloc(sizeof(uint8_t),size);
+	//ffmpeg::sws_scale(sws_ctx, (uint8_t const * const *)this->origframe->data,
+	//		 this->convertframe->linesize, 0, this->cod_context->height,
+	//		 pFrameRGB->data, pFrameRGB->linesize)
+	//ffmpeg::av_image_copy_to_buffer(buffer,size, this->convertframe->data, this->convertframe->linesize, ffmpeg::AV_PIX_FMT_RGBA64, this->convertframe->width, this->convertframe->height, 0);
+	//this->streamsout[0]->updateStream(new stream_data(buffer, size));
 	//this->packet.data=0;
 	//this->packet.size=0;
 	
